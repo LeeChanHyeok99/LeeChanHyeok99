9ee7485d7f0bcf39da00091d6432162f1dfbc88f
LCH99 <105422346+LeeChanHyeok99@users.noreply.github.com>
2022-09-19 12:14:30 +0900

    Add files via upload

---
+++ project003/js_home.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+$(".menu").click(function(){
+
+$(".slide_menu").addClass('on')
+
+
+})
+
+$(".m_menu").click(function(){
+
+    $(".slide_menu").removeClass('on')
+    
+    
+    })
+
+
+
+
+
+
+})
